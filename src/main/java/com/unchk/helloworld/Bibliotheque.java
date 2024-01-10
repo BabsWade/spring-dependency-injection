eca3b71a452005ceb624a4615e7797cb59dd8ad8
BabsWade <wadebabacar813@gmail.com>
2024-01-10 23:06:12 +0000

    Création de la classe Bibliothèque avec une méthode emprunterLivre

--- src/main/java/com/unchk/helloworld/Bibliotheque.java
+++ src/main/java/com/unchk/helloworld/Bibliotheque.java
@@ -1,5 +1,6 @@
 package com.unchk.helloworld;
 
+//Création de la classe Bibliothèque
 public class Bibliotheque {
 	public void emprunterLivre() {
 		System.out.println("Un livre vient d'être emprunté");
