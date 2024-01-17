cd0668fb564fd9e3c7e9af12acc4f62bf360bdf7
BabsWade <wadebabacar813@gmail.com>
2024-01-17 21:14:05 +0000

    Update

--- src/main/java/com/unchk/helloworld/model/Helloworld.java
+++ src/main/java/com/unchk/helloworld/model/Helloworld.java
@@ -4,15 +4,19 @@ package com.unchk.helloworld.model;
  * Modèle représentant un message "Hello World".
  */
 public class Helloworld {
-	private String value;
+private String value = "hello world";
 
-    public Helloworld() {
-        this.value = "hello world";
-    }
+public String getValue() {
+	return value;
+}
 
-    public Helloworld(String value) {
-        this.value = value;
-    }
+public void setValue(String value) {
+	this.value = value;
+}
+@Override
+public String toString() {
+	return value;
+}
 
 
 }
